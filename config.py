b46ee3bb49a45b64b91777c036015fc8cfefe234
dhavalanime <dhavalc393798@gmail.com>
2025-03-02 16:15:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.\n\n ᴅᴇᴠʟᴏᴘᴇʀ - @AcxAnime</b>")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6692613520").split()):
+    for x in (os.environ.get("ADMINS", "6692613520 7475464684").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
